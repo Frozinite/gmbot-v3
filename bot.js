67e929d03a1bba52da322c6620474e06b578f9f8
Frozinite <ryhuang@s-164-67-232-180.resnet.ucla.edu>
2017-02-28 21:24:12 -0800

    Add discord random quote support

--- bot.js
+++ bot.js
@@ -189,10 +189,11 @@ var temp_message2 = message;
   var regex2 = /^\!quote/i;
 
   if (regex2.test(temp_message2)){
-    var quote1 = "Barnes: It’s only cheating if you both finish";
-    bot.sendMessage({ to: channelID, message: quote1 });
+    var quote1 = " Barnes: It’s only cheating if you both finish ";
+    //bot.sendMessage({ to: channelID, message: quote1 });
     var quote = quotes.discordRandomQuote();
-    bot.sendMessage({ to: channelID, message: quote });
+    var quote2 = debug1.concat(quote, quote1);
+    bot.sendMessage({ to: channelID, message: quote2 });
     return true;
   }
 
