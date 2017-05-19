0b0716f173786ec470a6c69c128c163ad4dc5984
Frozinite <ryhuang@Ryans-MacBook-Pro.local>
2017-05-18 18:27:20 -0700

    Add war command functionality

--- custom_commands/war.js
+++ custom_commands/war.js
@@ -93,6 +93,11 @@ function cmdSaveWar(args) {
     var d = date.match(/^(\d{1,2})\/(\d{1,2})\/(\d{4})$/);
     if(d){
       date = new Date(d[3], d[1]-1, d[2]);
+
+      var year = date.getFullYear();
+      var month = date.getMonth() + 1;
+      var day = date.getDate();
+      date = month + "-" + day + "-" + year;
     }else{
       msg = "Incorrect date format.  Date must be in the format mm/dd/yyyy.";
       return msg;
@@ -110,7 +115,7 @@ function cmdSaveWar(args) {
     msg = "New war entry created!  " + host_clan + " will be hosting the " + war_name + " war on " + date + " at " + time + "\n";
     return msg;
   } else {
-    return "Failed to create a new war.  The proper command is \“!war save <host_clan> <opponent> <date> <time> <timezone>\”.  Date must be in the format mm/dd/yyyy.  All other arguments can be in any format, but must be one word.  For example: \“!war new mutiny EE 3/14/2017 5pm EST\” or \“!war new RT potluck 6/23/2017 5pm EST\”\n";
+    return "Failed to create a new war entry.  The proper command is \“!war new <host_clan> <opponent> <date> <time> <timezone>\”.  Date must be in the format mm/dd/yyyy.  All other arguments can be in any format, but must be one word.  For example: \“!war new mutiny EE 3/14/2017 5pm EST\” or \“!war new RT potluck 6/23/2017 5pm EST\”\n";
   }
 }
 
