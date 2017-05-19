dd779b306b26a6e1d93245c9644578eb6b416187
Frozinite <ryhuang@Ryans-MacBook-Pro.local>
2017-05-18 18:07:10 -0700

    Add war command functionality

--- custom_commands/war.js
+++ custom_commands/war.js
@@ -94,7 +94,7 @@ function cmdSaveWar(args) {
     if(d){
       date = new Date(d[3], d[2]-1, d[1]);
     }else{
-      msg = "Incorrect date format.  Date must be in the format dd/mm/yyyy.";
+      msg = "Incorrect date format.  Date must be in the format mm/dd/yyyy.";
       return msg;
     }
 
@@ -107,7 +107,7 @@ function cmdSaveWar(args) {
     }
 
     //saveWar(warHash, callback);
-    msg = "War saved!";
+    msg = "New war entry created!  " + host_clan + " will be hosting the " + war_name " war on " + date + " at " + time + "\n";
     return msg;
   } else {
     return "Failed to create a new war.  The proper command is \“!war save <host_clan> <opponent> <date> <time> <timezone>\”.  Date must be in the format mm/dd/yyyy.  All other arguments can be in any format, but must be one word.  For example: \“!war new mutiny EE 3/14/2017 5pm EST\” or \“!war new RT potluck 6/23/2017 5pm EST\”\n";
